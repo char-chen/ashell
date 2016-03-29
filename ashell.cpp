2dae65e9b0de2b6aff51e58b619b34f27a6bdd26
Qingyang Wu <wuqy1203@pc57.cs.ucdavis.edu>
2016-03-29 10:40:58 -0700

    3.29 version

--- ashell.cpp
+++ ashell.cpp
@@ -9,11 +9,10 @@
 using namespace std;
 
 void prompt(const char* wd) {
-    string result = ""; //allocate space for wd
+    string result = ""; 
     
 	if (strlen(wd)>16) {
-        result.assign("/.../", 5);
-        
+        result.assign("/.../", 5);       
 		char temp[strlen(wd)];
 		int count1 = strlen(wd)-1;
 		int count2 = 0;
@@ -22,38 +21,53 @@ void prompt(const char* wd) {
 		while(c!='/') {
 			temp[count2++] = c;
 			c = wd[--count1];
-		}
+		} //check last /
 		
 		for (int i=0; i<(count2/2);i++) {
 			c = temp[i];
 			temp[i] = temp[count2-1-i];
 			temp[count2-1-i] = c;
-		}
+		} //reverse the string
+
 		result += temp;
 	}
-	else
+	else //if length < 16
         result.assign(wd);
     
-    result += "% ";
-	write(STDOUT_FILENO, result.c_str(), result.length());
-}
+    result += "% "; 
+	write(STDOUT_FILENO, result.c_str(), result.length()); //output the prompt
+} //output the prompt
 
 int main() {
 	char *wd = getcwd(NULL,0); //working directory
 	
 	while(1) {			
-        prompt(wd);
+        prompt(wd); //output the promt
         string input = "";
-        
-        while(1) {
-            char c = 0;
-            scanf("%c",&c);
+
+        char c = 0;
+
+        while(read(STDIN_FILENO, &c, 1) != -1) {
+
             if (c == '\n')
                 break;
             else
                 input += c;
-        }
-	}
+        } //read in input line
+		
+		if (input.length()==0)
+			continue;
+
+		if (input == "exit")
+			exit(0);
+		else
+		{
+			write(STDOUT_FILENO, "Failed to execute ", 18);
+			write(STDOUT_FILENO, input.c_str(), input.length());
+			write(STDOUT_FILENO, "\n", 1);
+		}//default situation
+
+	} //keep working until exit
 	
-	return 0;
+	return 0; //return 0;
 }
