a28c781fe81e1cbcb0efb1a2496354289cba448d
Will <will@campus-007-080.ucdavis.edu>
2016-03-31 14:39:34 -0700

    ss

--- ashell.cpp
+++ ashell.cpp
@@ -4,25 +4,29 @@
 #include <iostream>
 
 
-void cd(string dir)
+void cd(char *dir)
 {
-    if (dir == "") {
+    if (dir == NULL) {
         if (chdir(getenv("HOME")) == -1) {
             write(STDOUT_FILENO, "Error changing directory.\n", 26);
         }
     } else {
-        if (chdir(dir.c_str()) == -1) {
+        if (chdir(dir) == -1) {
             write(STDOUT_FILENO, "Error changing directory.\n", 26);
         }
     }
 }
 
-void ls(string dir) {
-    struct dirent *myfile;
+void ls(char* dir) {
+    char **args = new char*[2];
+    args[0] = "ls";
+    args[1] = "-al";
+    execvp(args[0], args);
+    /*struct dirent *myfile;
     struct stat fileStat;
     DIR *mydir;
-    if (dir != "")
-        mydir = opendir(dir.c_str());
+    if (dir != NULL)
+        mydir = opendir(dir);
     else
         mydir = opendir(".");
     
@@ -46,19 +50,34 @@ void ls(string dir) {
         }
         closedir(mydir);
     } else {
-        write(STDOUT_FILENO, ("Failed to open directory \"" + dir).c_str(), 25 + dir.length());
+        string temp(dir);
+        write(STDOUT_FILENO, ("Failed to open directory \"" + temp).c_str(), 25 + temp.length());
         write(STDOUT_FILENO, "\"\n", 2);
-    }
+    }*/
 }
 
-void ff(string filename, string directory)
+void ff(char* filename, char* directory)
 {
     
 }
 
-void execBuildIn(char* command, char **args)
+void execBuildIn(char* str, char **args)
 {
-    cout << "try to exec build-in" << endl;
+    string command(str);
+    
+    if (command == "exit") {
+        exit(0);
+    } else if (command == "cd") {
+        cd(args[1]);
+    } else if (command == "ls") {
+        ls(args[1]);
+    } else if (command == "ff") {
+        ff(args[1], args[2]);
+    } else if (command == "pwd") {
+        char *wd = getcwd(NULL, 0);
+        write(STDOUT_FILENO, wd, strlen(wd));
+        write(STDOUT_FILENO, "\n", 1);
+    }
 }
 
 
@@ -67,16 +86,21 @@ void execBuildIn(char* command, char **args)
 
 int main()
 {
-    char *wd = getcwd(NULL, 0); //working directory
+    
     history *h = new history(); //init history struct
     
     while(true) {
         int status;
+        char *wd = getcwd(NULL, 0); //working directory
         printPrompt(wd); //output the promt
         
         string input = mygetline(h); //read and store commands
+        
+        if (input == "")
+            continue;
+        
         char **args = getCommand(input);
-
+        
         if (isBuildIn(args[0])) {
             execBuildIn(args[0], args);
         } else {
@@ -87,7 +111,7 @@ int main()
                 waitpid(-1, &status, 0);
             } else {
                 //Child code
-                if (execv(args[0], args) == -1) {
+                if (execvp(args[0], args) == -1) {
                     write(STDOUT_FILENO, "Failed to execute ", 18);
                     write(STDOUT_FILENO, args[0], strlen(args[0]));
                     write(STDOUT_FILENO, "\n", 1);
